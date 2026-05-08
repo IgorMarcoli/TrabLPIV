cbea039dbf95db546d35a6e7d1279afac5ba3e36
igor_MB <igormarcolibastos.imb@gmail.com>
2026-05-08 16:37:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,22 @@
       <p>Avaliações, rankings e recomendações para leitores.</p>
     </div>
 </section>
+<main class="container">
+    <h2 class="section-title">Livros em destaque</h2>
 
-<section>
+    <div class="books">
 
-</section>
+      <div class="card">
+        <img src="C:\Users\Fatec\Desktop\imagens.jpg" alt="Livro">
+        
+        <div class="card-content">
+          <h3>Dom Casmurro</h3>
+          <div class="rating">⭐ 9.1</div>
+
+          <p class="description">
+            Um clássico da literatura brasileira escrito por Machado de Assis.
+          </p>
+        </div>
+      </div>
 </body>
 </html>
