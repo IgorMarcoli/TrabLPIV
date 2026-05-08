a85ed0d4691f1d52457959b2d7c2cf0dac39a517
igor_MB <igormarcolibastos.imb@gmail.com>
2026-05-08 16:28:36 -0300

    vai se fuder porra

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Divulgação</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+    <header>
+    <div class="logo">Book</div>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Livros</a>
+        <a href="#">Categoria</a>
+        <a href="#">Contato</a>
+    </nav>
+</header>
+<hr>
+<section class="banner">
+    <div class="banner-content">
+      <h1>Descubra novos livros</h1>
+      <p>Avaliações, rankings e recomendações para leitores.</p>
+    </div>
+</section>
+</body>
+</html>
